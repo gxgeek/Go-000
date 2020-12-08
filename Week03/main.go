ab8cea449a77f6a73ee74551fec6bb0ff0599100
gaoxing <gaoxinggaoxing@didiglobal.com>
2020-12-08 15:13:43 +0800

    完成 server 优雅关闭

--- Week03/main.go
+++ Week03/main.go
@@ -3,111 +3,84 @@ package main
 import (
 	"context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-
-//
-//func main() {
-//	var once sync.Once
-//	for i := 0; i < 5; i++ {
-//		go func(i int) {
-//			fun1 := func() {
-//				fmt.Printf("i:=%d\n", i)
-//			}
-//			once.Do(fun1)
-//		}(i)
-//	}
-//	// 为了防止主goroutine直接运行完了，啥都看不到
-//	time.Sleep(50 * time.Millisecond)
-//}
-
-//func main() {
-//	var once sync.Once
-//	fun1 := func() {
-//		fmt.Println("第一次打印")
-//	}
-//	once.Do(fun1)
-//
-//	fun2 := func() {
-//		fmt.Println("第二次打印")
-//	}
-//
-//	once.Do(fun2)
-//}
-
-
-func main() {
-	//ctx, cancel := context.WithCancel(context.Background())
-	//go watch("【监控1】",ctx)
-	//go watch(ctx,"【监控2】")
-	//go watch(ctx,"【监控3】")
-	//ctx =context.(ctx,"key","【22222】")
-	//
-	//time.Sleep(10 * time.Second)
-	//fmt.Println("可以了，通知监控停止")
-	//ctx =context.WithValue(ctx,"key","【333333】")
-	//cancel()
-	//ctx =context.WithValue(ctx,"key","【44444444】")
-	////为了检测监控过是否停止，如果没有监控输出，就表示停止了
-	//time.Sleep(5 * time.Second)
-}
-func watch( name string,ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println(name,"监控退出，停止了...",ctx.Value("key"))
-			return
-		default:
-			fmt.Println(name,"goroutine监控中...",ctx.Value("key"))
-			time.Sleep(2 * time.Second)
-		}
-	}
-}
-
-
-var Count int = 0
-func main1()  {
-
-
-		//g := new(errgroup.Group)
-		//var urls = []string{
-		//	"http://www.golang.org/",
-		//	"http://www.google.com/",
-		//	"http://www.somestupidname.com/",
-		//}
-		//for _, url := range urls {
-		//	// Launch a goroutine to fetch the URL.
-		//	url := url // https://golang.org/doc/faq#closures_and_goroutines
-		//	g.Go(func() error {
-		//		// Fetch the URL.
-		//		resp, err := http.Get(url)
-		//		if err == nil {
-		//			resp.Body.Close()
-		//		}
-		//		return err
-		//	})
-		//}
-		//// Wait for all HTTP fetches to complete.
-		//if err := g.Wait(); err == nil {
-		//
-		//	fmt.Println("Successfully fetched all URLs.")
-		//}else {
-		//	fmt.Println(g.Wait().Error())
-		//}
-
-	//for i:= 0; i <100;i++ {
-	//	CountAdd()
-	//}
-	//value := atomic.Value{}
-	//value.
-	//time.Sleep(100 * time.Millisecond)
-	//fmt.Println(Count)
-}
-
-func CountAdd() {
-	go func() {
-		time.Sleep(2 * time.Millisecond)
-		Count = Count+1
+func main()  {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func() {
+		log.Println("global defer")
+		cancel()
 	}()
+	g, _ := errgroup.WithContext(ctx)
+	// server
+	g.Go(func() error {
+		return runServer(ctx,8001,"正式server")
+	})
+	// debug server
+	g.Go(func() error {
+		return runServer(ctx,8111,"debug server")
+	})
+	c := make(chan os.Signal)
+	signal.Notify(c,os.Interrupt)
+	g.Go(func() error {
+		return listenStopSignal(ctx,c,cancel)
+	})
+	log.Printf("Actual pid is %d", syscall.Getpid())
+	if err := g.Wait(); err != nil {
+		log.Println("g.wait finish")
+		log.Printf("%+v",err)
+	}
+	log.Println("sever finish")
+	time.Sleep(2 * time.Second)
+
+}
+
+func listenStopSignal(ctx context.Context, sig chan os.Signal, cancel context.CancelFunc) error {
+	select {
+	case sign := <- sig:
+		log.Println("接受信号",sign)
+		err := fmt.Errorf("handle signal: %d", sign)
+		cancel()
+		return err
+	case <-ctx.Done():
+		log.Println("listenStopSignal ctx.Done")
+		return nil
+	}
+
+}
+func runServer(ctx context.Context, serverPort int, serverName string) error {
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", serverPort),
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("%+v",err)
+			log.Println(serverName,"into defer recover()")
+			cancelServer(s,serverName)
+		}
+	}()
+	go func() {
+		<-ctx.Done()
+		cancelServer(s,serverName)
+	}()
+	log.Println(serverPort,serverName," with run")
+	err := s.ListenAndServe()
+	log.Println(serverName,"with error",err)
+	return err
+}
+
+func cancelServer(s *http.Server, serverName string) {
+	log.Println(serverName,"stop")
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(timeoutCtx)
 }
